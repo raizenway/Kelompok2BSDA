d547414e225388b71fa7d0235ef5ccd7c7d45dd2
Banteng Harisantoso <117459151+raizenway@users.noreply.github.com>
2023-04-21 11:07:01 +0700

    menu_addsub

--- BodyHimpunan.cpp
+++ BodyHimpunan.cpp
@@ -20,8 +20,8 @@ void create_super (superset_list *S){
 }
 
 //pembentukan list subset-1
-void CreateSub (subset_list *H1){
-    H1->first_sub1 = NULL;
+void create_sub (subset_list *H1){
+    H1->first_sub = NULL;
 }
 
 //mengecek apakah superset kosong
@@ -31,7 +31,7 @@ bool IsSuperEmpty (superset_list S){
 
 //mengecek apakah subset-1 kosong
 bool IsSubEmpty (subset_list H1){
-    return (H1.first_sub1 == NULL);
+    return (H1.first_sub == NULL);
 }
 
 // Memeriksa apakah mahasiswa dengan nama dan NIM yang diberikan sudah terdaftar dalam list super
@@ -74,7 +74,7 @@ void add_membersuper(superset_list *S, char nama_mhs[], char NIM[]) {
     }
     else{
     // Mengecek apakah daftar kosong
-    if (IsSuperEmpty(S)) {
+    if (IsSuperEmpty(*S)) {
         // Jika daftar kosong, maka node baru akan menjadi head
         S->first_super = P;
         S->first_super->next_sp = NULL;
@@ -94,10 +94,11 @@ void add_membersuper(superset_list *S, char nama_mhs[], char NIM[]) {
 }
 /* Menambah member subset*/
 void add_membersub(subset_list *H1, char nama_mhs[], char NIM[]){
+    
     alamatsub P;
     P = (alamatsub) malloc(sizeof(elmtsubset));
-    strcpy(P->nama_sb, nama_mhs);
-    strcpy(P->id_sb, NIM); 
+    strcpy(P->member_sub, nama_mhs);
+    strcpy(P->id_sub, NIM); 
     P->next_sub = NULL;
     //Mengecek apakah memori penuh
     if(IsFull()){
@@ -105,7 +106,7 @@ void add_membersub(subset_list *H1, char nama_mhs[], char NIM[]){
     }
     else{
     // Mengecek apakah daftar kosong
-    if (IsSubEmpty(H1)) {
+    if (IsSubEmpty(*H1)) {
         // Jika daftar kosong, maka node baru akan menjadi head
         H1->first_sub = P;
         H1->first_sub->next_sub = NULL;
@@ -125,29 +126,30 @@ void add_membersub(subset_list *H1, char nama_mhs[], char NIM[]){
 }
 
 int PilihUKM(){
-    int opsi;
-    while((opsi<1)&&(opsi>10)){
-    printf("Pilih salah satu UKM di bawah!\n
-            1. DKM\n
-            2. MUSKING\n
-            3. BADMINTON\n
-            4. BASKET\n
-            5. VOLI\n
-            6. POLBAN CHESS\n
-            7. JFP\n
-            8. ROBOTIK\n
-            9. FELLAS\n
-            10. FUTSAL\n");
-    scanf("%d", opsi);
-    if((opsi<1)&&(opsi>10)){
-        printf("Opsi tidak valid!");
-        system("pause");
-        system("cls");
-    }
-    }
+    int opsi = 0;
+    do{
+        printf("Pilih salah satu UKM di bawah!\n"
+               "1. DKM\n"
+               "2. MUSKING\n"
+               "3. BADMINTON\n"
+               "4. BASKET\n"
+               "5. VOLI\n"
+               "6. POLBAN CHESS\n"
+               "7. JFP\n"
+               "8. ROBOTIK\n"
+               "9. FELLAS\n"
+               "10. FUTSAL\n");
+        scanf("%d", &opsi);
+        if((opsi < 1) || (opsi > 10)){
+            printf("Opsi tidak valid!");
+            system("pause");
+            system("cls");
+        }
+    } while((opsi < 1) || (opsi > 10));
     
     return opsi;
-}
+}   
+
 
 void Seleksi_MemberSuper(superset_list *S)
 /*  AUTHOR      : Banteng Harisantoso
@@ -259,28 +261,185 @@ void Seleksi_MemberSuper(superset_list *S)
     syarat = true;
     }
     
-
 }
 
-void menu_addsub(subset_list *H1);
+void menu_addsub()
 /*  AUTHOR      : Banteng Harisantoso
     IS          : Sudah terbentuk list subset
     FS          : Menampilkan pesan berhasil atau gagal menambahkan mahasiswa ke dalam himp
     DESKRIPSI   : Prosedur untuk masuk ke menu penambahan mahasiswa ke pilihan UKM
 =======================================================================================================================*/
 {
-    printf("============================ Menu Pendataan UKM Mahasiswa ===================================");
-    PilihUKM();
-    switch (PilihUKM())
-    {
-    case 1:
-        a
-        break;
-    
-    default:
-        break;
-    }
+printf("============================ Menu Pendataan UKM Mahasiswa ===================================\n");
+char nama_mhs[61];
+char NIM[9];
+int opsi;
 
+PilihUKM();
+bool valid = false;
+while(!valid){
+    switch (opsi) {
+    case 1:
+        //Check NULL dulu sebelum declare
+        subset_list DKM;
+        create_sub(&DKM);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub(&DKM, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di DKM!\n");
+        printf("%s\n", DKM.first_sub->member_sub);
+        printf("%s", DKM.first_sub->id_sub);
+        system("pause");
+        system("cls");
+        valid = true;
+    break;
+    case 2:
+        subset_list MUSKING;
+        create_sub(&MUSKING);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub(&MUSKING, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di MUSKING!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break;
+
+    case 3:
+        subset_list BADMINTON;
+        create_sub(&BADMINTON);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub(&BADMINTON, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di BADMINTON!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break;
+
+    case 4:
+        subset_list BASKET;
+        create_sub( &BASKET);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub( &BASKET, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di BASKET!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break; 
+
+    case 5:
+        subset_list VOLI;
+        create_sub( &VOLI);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub( &VOLI, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di VOLI!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break; 
+
+    case 6:
+        subset_list POLBAN_CHESS;
+        create_sub( &POLBAN_CHESS);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub( &POLBAN_CHESS, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di ROBOTIK!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break; 
+
+    case 7:
+        subset_list JFP;
+        create_sub( &JFP);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub( &JFP, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di JFP!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break;
+
+    case 8:
+        subset_list ROBOTIK;
+     
+        create_sub( &ROBOTIK);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub( &ROBOTIK, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di POLBAN CHESS!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break;
+
+    case 9:
+        subset_list FELLAS;
+     
+        create_sub( &FELLAS);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub( &FELLAS, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di FELLAS!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break; 
+
+    case 10:
+        subset_list FUTSAL;
+        create_sub( &FUTSAL);
+        addnama(nama_mhs);
+        addnim(NIM);
+        add_membersub( &FUTSAL, nama_mhs, NIM);
+        printf("\nMahasiswa berhasil bergabung di FUTSAL!\n");
+        system("pause");
+        system("cls");
+        valid = true;
+        break; 
+
+    default:
+        printf("Masukkan opsi yang valid!\n");
+    }
+    }
+    
+}
+
+void addnama(char* nama_mhs){
+   
+    bool valid = false;
+   
+    while(!valid){
+        printf("Masukkan nama mahasiswa:\n");
+        scanf("%s", nama_mhs);
+        if(strlen(nama_mhs) > 60){
+             printf("Nama mahasiswa terlalu panjang!\n");
+        }
+        else{
+            valid = true;
+        }
+    }
+}
+
+
+void addnim(char* NIM){
+    bool valid = false;
+
+    while(!valid){
+        printf("Masukkan NIM: ");
+        scanf("%s", NIM);
+        if(strlen(NIM) > 9){
+            printf("NIM terlalu panjang!\n");
+        }
+        else{
+            valid = true;
+        }
+    }
 }
 
 void display_menu() {
