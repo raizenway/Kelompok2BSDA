5ae1288f1e29de3d049e2286d6512eca913163d1
bobmnueel <123434936+bobmnueel@users.noreply.github.com>
2023-04-25 08:46:01 +0700

    Update BodyNRLL.c
    update delete

--- BodyNRLL.c
+++ BodyNRLL.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+/*  File        : BodyNRSL.cpp 															 
+    Deskripsi   : Body prototype ADT aplikasi himpunan untuk pendataan UKM di Polban (NRLL) 
+    Author      : Kelompok 2B												                
+    Tanggal     : 16 APRIL 2023															  
+===================================================================================================*/
+#include <limits.h>
+#include <malloc.h>
 #include "SpHimpunan.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include <windows.h>
 
-/****** BODY SUB PROGRAM *******/
+/********** BODY SUB PROGRAM ***********/
 
 //pembentukan list superset
 void create_super (superset_list *S){
@@ -12,13 +20,8 @@ void create_super (superset_list *S){
 }
 
 //pembentukan list subset-1
-void CreateSub1 (subset1_list *H1){
-    H1->first_sub1 = NULL;
-}
-
-//pembentukan list subset-2
-void Createsub2 (subset2_list *H2){
-    H2->first_sub2 = NULL;
+void CreateSub (subset_list *H1){
+    H1->first_sub = NULL;
 }
 
 //mengecek apakah superset kosong
@@ -27,31 +30,307 @@ bool IsSuperEmpty (superset_list S){
 }
 
 //mengecek apakah subset-1 kosong
-bool IsSubAEmpty (subset1_list H1){
-    return (H1.first_sub1 == NULL);
+bool IsSubEmpty (subset_list H1){
+    return (H1.first_sub == NULL);
 }
 
-//mengecek apakah subset-2 kosong
-bool IsSubBEmpty (subset2_list H2){
-    return (H2.first_sub2 == NULL);
+// Memeriksa apakah mahasiswa dengan nama dan NIM yang diberikan sudah terdaftar dalam list super
+bool IsExistSuper(superset_list S, char nama_mhs[], char NIM[]) {
+    alamatsuper P;
+	P = S.first_super;
+    while (P != NULL) {
+        if (strcmp(P->member_sp, nama_mhs) == 0 && strcmp(P->id_member, NIM) == 0) {
+            return true;
+        }
+        P = P->next_sp;
+    }
+    return false;
 }
 
 //mengecek apakah proses alokasi berhasil atau gagal karena memori yang kurang
-// alamatsuper IsSuperFull (){
-//     alamatsuper bantu;
-
-//     bantu = (alamatsuper) malloc (sizeof(elmtsuper));
-//     if ( bantu != NULL){
-//         printf("Alokasi Memori Berhasil !");
-//     } else { printf("Alokasi Memori Gagal !");}
-// }
-
-bool IsSuperFull (){
-    /*kamus lokal*/
+bool IsFull() {
     alamatsuper bantu;
 
     bantu = (alamatsuper) malloc (sizeof(elmtsuper));
+    
+    if (bantu != NULL) {
+        return false;
+    } else { 
+		return true;
+	}
+}
+
+// Meminta input dari pengguna dan menambahkan mahasiswa ke dalam superset_list
+void add_membersuper(superset_list *S, char nama_mhs[], char NIM[]) {
+    alamatsuper P;
+    P = (alamatsuper) malloc(sizeof(elmtsuper));
+    strcpy(P->member_sp, nama_mhs);
+    strcpy(P->id_member, NIM);
+    P->next_sp = NULL;
+
+    //Mengecek apakah memori penuh
+    if(IsFull()){
+        printf("Memori penuh!\n");
+    }
+    else{
+    // Mengecek apakah daftar kosong
+    if (IsSuperEmpty(*S)) {
+        // Jika daftar kosong, maka node baru akan menjadi head
+        S->first_super = P;
+        S->first_super->next_sp = NULL;
+    }
+    else {
+        // Menambahkan node baru ke akhir list
+        alamatsuper last;
+		last = S->first_super;
+        while (last->next_sp != NULL) {
+            last = last->next_sp;
+        }
+        last->next_sp = P;
+        last->next_sp->next_sp = NULL;
+    }
+    }
+    
+}
+/* Menambah member subset*/
+void add_membersub(subset_list *H1, char nama_mhs[], char NIM[]){
+    alamatsub P;
+    P = (alamatsub) malloc(sizeof(elmtsubset));
+    strcpy(P->member_sub, nama_mhs);
+    strcpy(P->id_sub, NIM); 
+    P->next_sub = NULL;
+    //Mengecek apakah memori penuh
+    if(IsFull()){
+        printf("Memori penuh!\n");
+    }
+    else{
+    // Mengecek apakah daftar kosong
+    if (IsSubEmpty(*H1)) {
+        // Jika daftar kosong, maka node baru akan menjadi head
+        H1->first_sub = P;
+        H1->first_sub->next_sub = NULL;
+    }
+    else {
+        // Menambahkan node baru ke akhir list
+        alamatsub last;
+		last = H1->first_sub;
+        while (last->next_sub != NULL) {
+            last = last->next_sub;
+        }
+        last->next_sub = P;
+        last->next_sub->next_sub = NULL;
+    }
+    }
+    
+}
+
+int PilihUKM(){
+    int opsi;
+    while((opsi<1)&&(opsi>10)){
+    printf("Pilih salah satu UKM di bawah!\n"
+            "1. DKM\n"
+            "2. MUSKING\n"
+            "3. BADMINTON\n"
+            "4. BASKET\n"
+            "5. VOLI\n"
+            "6. POLBAN CHESS\n"
+            "7. JFP\n"
+            "8. ROBOTIK\n"
+            "9. FELLAS\n"
+            "10.FUTSAL\n");
+    scanf("%d", opsi);
+    if((opsi<1)&&(opsi>10)){
+        printf("Opsi tidak valid!");
+        system("pause");
+        system("cls");
+    }
+    }
+    
+    return opsi;
+}
+
+void Seleksi_MemberSuper(superset_list *S)
+/*  AUTHOR      : Banteng Harisantoso
+    IS          : Sudah terbentuk list superset
+    FS          : Menampilkan pesan berhasil atau gagal menambahkan mahasiswa ke dalam list POLBAN
+    DESKRIPSI   : Prosedur untuk masuk ke menu penambahan mahasiswa dengan syarat-syarat POLBAN,
+                  pengguna akan diarahkan ke switch, jika memilih 1 berturut-turut maka calon mahasiswa dapat menjadi mahasiswa
+                  POLBAN. Namun, jika mahasiswa tidak lolos salah satu persyaratan maka calon mahasiswa belum bisa menjadi
+                  mahasiswa POLBAN
+=======================================================================================================================*/
+{
+    char nama_mhs[20];
+    int jumlah;
+    int opsi;
+    bool syarat = true;
+    
+    printf("Berapa banyak calon mahasiswa yang ingin didata?\n");
+    scanf("%d", &jumlah);
+    while((jumlah == 0)||(jumlah<0)){
+        printf("Jumlah tidak valid! Silahkan masukkan jumlah yang valid.\n");
+        scanf("%d", &jumlah);
+    }
+
+    for(int i=0;i<jumlah;i++){
+    system("cls");
+    printf("Silahkan masukkan nama calon mahasiswa.\n");
+    scanf("%s", &nama_mhs);
+    while(syarat){
+            system("cls");
+            printf("Apakah calon mahasiswa tersebut lolos seleksi SNMPTN/SBMPTN atau SNMPN/SBMPN atau Ujian Mandiri?\n");
+            printf("1. Ya\n2. Tidak\n");
+            scanf("%d", &opsi);
+            switch (opsi)
+            {
+            case 1 :
+                while(syarat){
+                printf("\n\nApakah calon mahasiswa tersebut lolos seleksi berkas administrasi?\n");
+                printf("1. Ya\n2. Tidak\n");
+                scanf("%d", &opsi);
+                    switch (opsi)
+                    {
+                    case 1:
+                        while(syarat){
+                        printf("\n\nApakah calon mahasiswa tersebut sudah melunasi pembayaran UKT awal?\n");
+                        printf("1. Ya\n2. Tidak\n");
+                        scanf("%d", &opsi);
+                            switch (opsi)
+                            {
+                            case 1 : ;
+                                char NIM[10];
+                                system("cls");
+                                printf("Mahasiswa lolos persyaratan POLBAN!\n");
+                                printf("Silahkan masukkan NIM mahasiswa tersebut!\n");
+                                printf("NIM\t:");
+                                scanf("%s", &NIM);
+                                if(IsExistSuper(*S, nama_mhs, NIM)){
+                                   system("cls"); 
+                                   printf("\n\nMahasiswa tersebut sudah terdaftar di POLBAN!");
+                                   system("pause");
+                                }
+                                else{
+                                    add_membersuper(S, nama_mhs, NIM);
+                                    system("cls");
+                                    printf("Calon mahasiswa telah resmi menjadi mahasiswa POLBAN!");
+                                    printf("\nInformasi Mahasiswa:");
+                                    printf("\nNama Mahasiswa\t: %s", nama_mhs);
+                                    printf("\nNIM\t\t: %s\n", NIM);
+                                    system("pause");
+                                }
+
+                                syarat = false;
+                                break;
+
+                            case 2 :
+                            printf("\nMahasiswa belum memenuhi persyaratan!");
+                            system("pause");
+                            syarat = false;
+                            break;
+
+                            default:
+                                printf("\nMasukkan opsi yang valid!");
+                                system("pause");
+                            }
+                        }
+                    break;
+                    case 2 :
+                        printf("\nMahasiswa belum memenuhi persyaratan!");
+                        system("pause");
+                        syarat = false;
+                        break;
+                    default:
+                        printf("\nMasukkan opsi yang valid!");
+                        system("pause");
+                    }
+                }
+
+            break;
+            case 2 :
+               printf("\nMahasiswa belum memenuhi persyaratan!");
+               system("pause");
+               syarat = false;
+               break;
+            default:
+                printf("\nMasukkan opsi yang valid!");
+                system("pause");
+            }
+
+    }
+    syarat = true;
+    }
+}
+
+void del_membersuper(superset_list *S, char nama_mhs[], char NIM[]){
+    /*  AUTHOR  : Bob Manuel
+    IS          : List superset sudah terbentuk dan memiliki isi sebagai anggota superset
+    FS          : List yang berisi nama dan NIM yang ingin dihapus berhasil dihapus
+    DESKRIPSI   : Prosedur untuk mencari mahasiswa pada superset maupun subset.
+                  Pengguna akan diarahkan untuk menginput nama dan NIM dari mahasiswa yang ingin dicari.
+                  Jika nama dan NIM ditemukan maka modul akan mengembalikan nilai true, 
+                  sebaliknya jika nama dan NIM tidak ditemukan maka akan mengembalikan nilai false
+    =======================================================================================================================*/
+    //Kamus Lokal
+    alamatsuper bantu, hapus, next;
+    bantu = S->first_super;
+
+    if(IsExistSuper(*S, nama_mhs, NIM)){
+        while((bantu->next_sp->member_sp != nama_mhs) || (bantu != NULL)){
+            bantu= bantu->next_sp; 
+            if(bantu->next_sp->member_sp == nama_mhs){
+                hapus = bantu->next_sp;
+                next = hapus->next_sp;
+                bantu->next_sp = next;
+                printf("Mahasiswa bernama %s sudah berhasil dihapus", &hapus->member_sp);
+                free(hapus);
+            } 
+        }
+    } else {
+        printf("Mahasiswa tidak ditemukan");
+    }
+    
+}
+
+void del_membersub(subset_list *H1, char nama_mhs[], char NIM[]) {
+    alamatsub bantu, hapus, next;
+    bantu = H1->first_sub;
+
+    if(IsExistSub(*H1, nama_mhs, NIM)){
+        while((bantu->next_sub->member_sub != nama_mhs) || (bantu != NULL)){
+           bantu = bantu->next_sub;
+           if(bantu->next_sub->member_sub == nama_mhs){
+            
+           }
+        }
+    }
 }
 
 
-//menge
+
+void menu_addsub(subset_list *H1){
+    printf("============================ Menu Pendataan UKM Mahasiswa ===================================");
+    PilihUKM();
+    switch (PilihUKM())
+    {
+    case 1:
+        break;
+    
+    default:
+        break;
+    }
+
+}
+
+void display_menu() {
+    	printf("=========================================\n");
+        printf("|    Kalkulator Himpunan UKM POLBAN     |\n");
+        printf("=========================================\n");
+        printf("| Menu: \t\t\t\t|\n");
+        printf("| [1] Buat Himpunan \t\t\t|\n");
+        printf("| [2] Operasi Himpunan \t\t\t|\n");
+        printf("| [3] Edit Daftar Himpunan \t\t|\n");
+        printf("| [4] Cari Anggota Himpunan \t\t|\n");
+        printf("| [5] Tampilkan Daftar Himpunan \t|\n");
+        printf("| [99] Exit \t\t\t\t|\n");
+        printf("=========================================\n");
+
+}
