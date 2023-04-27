0ebdb07e8135ea8a3e5a2a12ea4f1664d28809ce
bobmnueel <123434936+bobmnueel@users.noreply.github.com>
2023-04-27 10:52:09 +0700

    Update SpHimpunan.h

--- SpHimpunan.h
+++ SpHimpunan.h
@@ -10,7 +10,7 @@
 /*============================ STRUKTUR DATA ============================*/
 typedef struct subset *alamatsub;
 typedef struct subset{
-    char nama_sub[10];
+    char nama_sub[20];
     char member_sub[61];
     char id_sub[10];
     alamatsub next_sub;
@@ -43,28 +43,37 @@ bool IsSuperEmpty(superset_list S);
 bool IsSubEmpty(subset_list H1);
 bool IsFull();
 void add_membersuper(superset_list *S, char nama_mhs[], char NIM[]);
-void add_membersub(subset_list *H1, char nama_mhs[], char NIM[]);
+void add_membersub(subset_list *H1, char nama_mhs[], char NIM[], char UKM[]);
 void Seleksi_MemberSuper(superset_list *S);
 void del_membersuper(superset_list *S, char nama_mhs[], char NIM[]);
 void del_membersub(subset_list *H1, char nama_mhs[], char NIM[]);
 void Penghentian_Studi(superset_list *S, char nama_mhs[], char NIM[]);
-void irisan(superset_list S, subset_list H1 );
-void gabungan(superset_list S, subset_list H1 ); 
-void komplemen(superset_list S, subset_list H1 );
-void selisih(superset_list S, subset_list H1 );
+void irisan(subset_list H1, subset_list H2);
+void gabungan(subset_list H1, subset_list H2); 
+void komplemen(superset_list S, subset_list H1, subset_list  H2 );
+void selisih(subset_list H1, subset_list H2);
 void append_history(superset_list S, subset_list H1 );
 void write_cur(superset_list S, subset_list H1);
 void write_guide();
-void display_cur();
+void display_cur(subset_list H);
 void display_menu();
 void display_search(superset_list S, subset_list H1, char nama_mhs[], char NIM[]);
 void display_guide();
 void display_histori();
-void menu_addsub(subset_list* DKM, subset_list* MUSKING, subset_list* BADMINTON, subset_list* BASKET, subset_list* VOLI, subset_list* POLBAN_CHESS, subset_list* JFP, subset_list* ROBOTIK, subset_list* FELLAS, subset_list* FUTSAL);
-int ListUKM();
-int OpsiUKM();
-void addnama(char* nama_mhs);
-void addnim(char* NIM);
-void ekstraklist(subset_list* H, subset_list DKM, subset_list MUSKING, subset_list BADMINTON, subset_list BASKET, subset_list VOLI, subset_list POLBAN_CHESS, subset_list JFP, subset_list ROBOTIK, subset_list FELLAS, subset_list FUTSAL);
-
+void MenuAddSub(superset_list POLBAN, subset_list* DKM, subset_list* KEWIRAUSAHAAN, subset_list* BADMINTON, subset_list* BASKET, subset_list* VOLI, subset_list* POLBAN_CHESS, subset_list* JFP, subset_list* ROBOTIK, subset_list* FELLAS, subset_list* USF);
+//Hari V
+int ListUKM(); 
+//Hari V
+void addnama(char* nama_mhs); 
+//Hari V
+void addnim(char* NIM); 
+//Hari V
+void MenuBuatHimpunan(superset_list* POLBAN, subset_list* DKM, subset_list* KEWIRAUSAHAAN, subset_list* BADMINTON, subset_list* BASKET, subset_list* VOLI, subset_list* POLBAN_CHESS, subset_list* JFP, subset_list* ROBOTIK, subset_list* FELLAS, subset_list* USF);
+//Hari V
+void MenuOperasi(subset_list* H1, subset_list* H2, subset_list DKM, subset_list KEWIRAUSAHAAN, subset_list BADMINTON, subset_list BASKET, subset_list VOLI, subset_list POLBAN_CHESS, subset_list JFP, subset_list ROBOTIK, subset_list FELLAS, subset_list USF);
+//Hari V
+void EkstrakList(subset_list* H, subset_list DKM, subset_list KEWIRAUSAHAAN, subset_list BADMINTON, subset_list BASKET, subset_list VOLI, subset_list POLBAN_CHESS, subset_list JFP, subset_list ROBOTIK, subset_list FELLAS, subset_list USF);
+//Hari V
+void CopyList(subset_list* H, subset_list SC); 
+//Hari V
 #endif /* SPHIMPUNAN_H */
