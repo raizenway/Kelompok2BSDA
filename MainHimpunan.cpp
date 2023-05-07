583f9fb30379c4e9f4575ac63b69212db958a63d
Banteng Harisantoso <117459151+raizenway@users.noreply.github.com>
2023-05-07 20:58:53 +0700

    Delete File, Histori

--- MainHimpunan.cpp
+++ MainHimpunan.cpp
@@ -63,10 +63,12 @@ int main(){
             case 3:
             	MenuEditHimpunan(&POLBAN, &DKM, &KEWIRAUSAHAAN, &BADMINTON, &BASKET, &VOLI, &POLBAN_CHESS, &JFP, &ROBOTIK, &FELLAS, &USF, nama_mhs, NIM); break;
             case 4 : EkstrakPOLBAN(&POLBAN); break;
-            case 5 : PanduanPenggunaan(); 
-            TampilkanPanduan(); break;
-            case 6 : search(POLBAN, DKM, KEWIRAUSAHAAN, BADMINTON, BASKET, VOLI, POLBAN_CHESS, JFP, ROBOTIK, FELLAS, USF, nama_mhs, NIM);
-        }
+            case 5 : display_cur(POLBAN); break;
+            case 6 : EkstrakUKM(POLBAN, &DKM, &KEWIRAUSAHAAN, &BADMINTON, &BASKET, &VOLI, &POLBAN_CHESS, &JFP, &ROBOTIK, &FELLAS, &USF); break;
+            case 7 : display_UKM(DKM);
+            case 8 : DeleteFromUKMFile("Banteng Harisantoso", "221524036", "DKM");
+            case 9 : DisplayHistory();
+           }
 
         printf("\n");
     } while (choice != 99);
